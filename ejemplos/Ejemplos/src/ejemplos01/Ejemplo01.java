2b8082887e7bf5ecfca881f907a016173f0da90e
unknown <sjmayon1@utpl.edu.ec>
2024-01-03 16:13:17 -0500

    ejercicio en clases

--- ejemplos/Ejemplos/src/ejemplos01/Ejemplo01.java
+++ ejemplos/Ejemplos/src/ejemplos01/Ejemplo01.java
@@ -18,20 +18,44 @@ public class Ejemplo01 {
         // TODO code application logic here
         
         
-        String miCiudad = obtenerCiudadMayuscula("Loja");
-        System.out.printf("%s\n", miCiudad);
+       // String miCiudad = obtenerCiudadMayuscula("Loja");
+        //System.out.printf("%s\n", miCiudad);
+        obtenerMultiplicacion(7,50);
+        
+        String miMensaje= obtenerMultiplicacionDos(7,50);
+        System.out.printf("%s\n",miMensaje);
     }
     
     
-    public String obtenerCiudadMayuscula(String m){
+    public static String obtenerCiudadMayuscula(String m){
         String m2 = m.toUpperCase();
         return m2;
     }
-    
-    public static void obtenerMultiplicacion(int tabla, int limite){
-        
+      public static String obtenerMultiplicacionDos(int tabla, int limite){
+        String cadenaFinal="";
+        int resultado=0;
+        for (int i = 1; i <= limite; i++) {
+           resultado = tabla * i;
+            cadenaFinal = String.format("%s%d * %d = %d\n",
+                    cadenaFinal, tabla, i, 
+                    resultado);
+
+    }
+    return cadenaFinal;
     }
     
+    public static void obtenerMultiplicacion(int tabla, int limite){
+        String cadenaFinal="";
+        int resultado=0;
+        for (int i = 1; i <= limite; i++) {
+           resultado = tabla * i;
+            cadenaFinal = String.format("%s%d * %d = %d\n",
+                    cadenaFinal, tabla, i, 
+                    resultado);
+
+    }
+    System.out.printf("%s\n", cadenaFinal);
+    }
     public static String obtenerNombre(){
         return "Luis";
     }
